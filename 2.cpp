0868692af8fc8faab7beffedebb0498041ae5cbb
RazvanDRL <60571589+RazvanDRL@users.noreply.github.com>
2021-06-10 20:29:58 +0300

    Create 2.cpp

---
+++ 2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int N;
+    cin>>N;
+    int v[100];
+    int i;
+    for (i = 1; i <= N; i++)
+        cin>>v[i];
+    int maxim = v[1];
+    for (i = 2; i <= N; i++)
+        if (maxim< v[i])
+            maxim = v[i];
+    cout<<maxim;
+    return 0;
+}
