95f4a5d5d561e6de6f7bd30a74991a970d22a06a
RazvanDRL <60571589+RazvanDRL@users.noreply.github.com>
2021-06-10 20:32:42 +0300

    Create 10.cpp

---
+++ 10.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin>>a>>b;
+    while (a!=b)
+    {
+        if (a>b)
+        {
+            a-=b;
+        }
+        else
+        {
+            b-=a;
+        }
+    }
+    cout<<"rezultat:"<<a;
+    return 0;
+}
